69221096d1e80c08b072fc2b302a53108be87602
Juan <juanmaurente@gmail.com>
2024-11-18 17:18:55 +1100

    creating grid with 4 Boxes

--- src/components/board/board.tsx
+++ src/components/board/board.tsx
@@ -3,11 +3,13 @@ import styles from './board.module.css';
 
 const board = () => {
 	return (
-		<div className={styles.board}>
-			<Box color={'red'} />
-			<Box color={'blue'} />
-			<Box color={'yellow'} />
-			<Box color={'green'} />
+		<div className={styles.container}>
+			<div className={styles.board}>
+				<Box color={'red'} />
+				<Box color={'blue'} />
+				<Box color={'yellow'} />
+				<Box color={'green'} />
+			</div>
 		</div>
 	);
 };
